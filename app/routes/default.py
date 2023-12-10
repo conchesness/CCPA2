fc58d530dcea2653062bb0a247431666fc9caccc
Conchesness <steve@conches.org>
2023-12-10 09:18:19 -0800

    redir index to wordcloud

--- app/routes/default.py
+++ app/routes/default.py
@@ -1,7 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 # This is for rendering the home page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('wordcloud'))
