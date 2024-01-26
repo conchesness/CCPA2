27b3b6ecc573b54d8346de612ad0d5a9f611948e
conchesness <steve@conches.org>
2024-01-26 13:00:38 -0800

    alumni map and navbar expose survey to teachers

--- app/routes/default.py
+++ app/routes/default.py
@@ -5,3 +5,7 @@ from flask import render_template, redirect, url_for
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/alumnimap')
+def alimnimap():
+    return render_template("alumnimap.html")
