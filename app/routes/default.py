2fa126ecad9d19156fc57aeaaf1f2621e1e9de19
conchesness <steve@conches.org>
2024-01-28 10:15:56 -0800

    attempt to integrate alumnimap

--- app/routes/default.py
+++ app/routes/default.py
@@ -6,6 +6,10 @@ from flask import render_template, redirect, url_for
 def index():
     return render_template('index.html')
 
+@app.route('/alumnimaptest')
+def alimnimaptest():
+    return render_template("alumnimap/index.html")
+
 @app.route('/alumnimap')
 def alimnimap():
     return render_template("alumnimap.html")
